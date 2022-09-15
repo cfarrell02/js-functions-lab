051b90bd5a5f82a2ffd7329a9936cf7731ca831a
Cian Farrell <73341912+cfarrell02@users.noreply.github.com>
2022-09-15 15:50:51 +0100

    Excercise 5

--- excercise3.js
+++ excercise3.js
@@ -1,8 +1,10 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(todos => {
-  const uncompleted = todos.filter(item => !item.completed && item.title!=undefined 
-    &&item.userID != undefined);
+  const uncompleted = todos.filter(item => !item.completed).map(
+    ({userId,title}) => {
+     return [userId,title]
+    });
   console.log(uncompleted);
   })  .catch(function(err) { 
     console.log(err);
--- excercise5.js
+++ excercise5.js
@@ -1,15 +1,15 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(todos => {
-  const count = todos.reduce((acc,element,index,array) => {
+  const uncompleted = todos.reduce((acc,element,index,array) => {
     if(element.completed){
-      acc[index] = [element.userId,element.title];
-      //return acc 
+      acc.push([element.userId,element.title]);
     }
     return acc
   },[])
-
-console.log(count)
+  console.log(uncompleted);
   })  .catch(function(err) { 
     console.log(err);
   });
+
+  
